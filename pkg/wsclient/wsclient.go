bebae5786da074004cdf3e0ebe1641837ae38be5
Martin Bozhilov <marogo142005@gmail.com>
2024-03-14 10:48:57 +0200

    [ADD] rpc ws client

---
+++ pkg/wsclient/wsclient.go
@@ -0,0 +1,18 @@
+package wsclient
+
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/pandishpanceta/contract-observer-service/pkg/config"
+)
+
+func InitWsClient(cfg *config.Config) (*ethclient.Client, error) {
+	client, err := ethclient.Dial(cfg.WSUrl)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return client, nil
+}
