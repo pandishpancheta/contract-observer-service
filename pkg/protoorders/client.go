7e9c39e9577215275e4153cee56667abd7fa468e
Martin Bozhilov <marogo142005@gmail.com>
2024-03-15 01:51:48 +0200

    [ADD] orders proto

---
+++ pkg/protoorders/client.go
@@ -0,0 +1,26 @@
+package protoorders
+
+import (
+	"log"
+
+	"github.com/pandishpancheta/contract-observer-service/pkg/config"
+	orders "github.com/pandishpancheta/contract-observer-service/pkg/protoorders/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type ServiceClient struct {
+	Client orders.OrderServiceClient
+}
+
+func InitServiceClient(cfg *config.Config) (orders.OrderServiceClient, error) {
+	creds := insecure.NewCredentials()
+
+	c, err := grpc.Dial(cfg.ListingsServiceClient, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("Failed to dial TokenizationService: %v", err)
+		return nil, err
+	}
+
+	return orders.NewOrderServiceClient(c), nil
+}
