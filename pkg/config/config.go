5a33f0fe8e699cb9974ff54befad892f0b369540
Martin Bozhilov <marogo142005@gmail.com>
2024-03-15 15:38:13 +0200

    [FIX]

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -14,9 +14,9 @@ type Config struct {
 
 func LoadConfig() Config {
 	return Config{
-		WSUrl:                 strings.TrimSpace(os.Getenv("WS_URL")),
-		ContractAddress:       strings.TrimSpace(os.Getenv("CONTRACT_ADDRESS")),
-		ListingsServiceClient: strings.TrimSpace(os.Getenv("LISTINGS_SERVICE_CLIENT")),
-		OrdersServiceClient:   strings.TrimSpace(os.Getenv("ORDERS_SERVICE_CLIENT")),
+		WSUrl:                 strings.TrimSuffix(os.Getenv("WS_URL"), "\n"),
+		ContractAddress:       strings.TrimSuffix(os.Getenv("CONTRACT_ADDRESS"), "\n"),
+		ListingsServiceClient: strings.TrimSuffix(os.Getenv("LISTINGS_SERVICE_CLIENT"), "\n"),
+		OrdersServiceClient:   strings.TrimSuffix(os.Getenv("ORDERS_SERVICE_CLIENT"), "\n"),
 	}
 }
